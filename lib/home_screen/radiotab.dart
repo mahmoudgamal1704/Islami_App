92ee9744e35f6f053f3c6c5e838e41eb83266d86
mahmoud <mahmoudgamal1704@gmail.com>
2022-11-26 19:57:02 +0200

    design radio tab without any function

--- lib/home_screen/radiotab.dart
+++ lib/home_screen/radiotab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islami/mytheme.dart';
 
 class RadioTab extends StatelessWidget {
   // const QuranTab({Key? key}) : super(key: key);
@@ -6,7 +7,26 @@ class RadioTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('assets/images/radiopic.png'),
+          Text(
+            'إذاعة القرآن الكريم',
+            style: Theme.of(context).textTheme.subtitle2,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.skip_previous,size: 50,color: MyTheme.colorGold,),
+              SizedBox(width: 20,),
+              Icon(Icons.play_arrow,size: 80,color: MyTheme.colorGold,),
+              SizedBox(width: 20,),
+              Icon(Icons.skip_next,size: 50,color: MyTheme.colorGold,),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
